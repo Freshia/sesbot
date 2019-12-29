aaedd7b1427e5411dea7778dde7024387fd81c64
Freshia <freshiasackey@gmail.com>
2019-12-29 13:18:51 +0300

    Added all candidates initialization

--- sesbot.cpp
+++ sesbot.cpp
@@ -2,11 +2,10 @@
 #include "sesbot.h"
 #include "Nextion.h"
 
+//remember to document course codes
 
 String courses [] = {"mecht","eee","civil","tie","marine","gegis","mech","abe","mining"};
-//NexText courses_text [] = {text_mecht,text_eee,text_civil,text_tie,text_marine,text_gegis,text_mech,text_abe,text_mining};
 String homepage_contents[] = {"settings","results","mail","vote","no_vote","no_vote1"};
-//NexButton homepage_buttons [] = {bn_settings,bn_results,bn_mail,bn_vote,bn_no_vote,bn_no_vote1};
 
 struct candidate{
     String name;
@@ -15,6 +14,14 @@ struct candidate{
     int results;
 };
 struct homepage_components_text{
+    String name;
+    NexText nexText;
+};
+struct coursepage_components_text{
+    String name;
+    NexText nexText;
+};
+struct candidate_components_text{
     String name;
     NexText nexText;
 };
@@ -39,13 +46,48 @@ void initialize_homepage(){
         int n = name_string.length();
         const char char_array[n+1];
         strcpy(char_array, name_string.c_str());
+
         components_text[i]-> nexText= NexText(0, i+1, char_array);
     }
 }
 void initialize_courses_page(){
     NexPage courses_page = NexPage(1, 0, "courses");
     int array_length = sizeof(courses)/sizeof(courses[0]);
-    for(int  i = 0; i<=array_length;i++){
-       // courses_text[i] = NexText(0, i+2, "bn_"+courses[i]);
+    coursepage_components_text *components_text [array_length];
+     for(int  i = 0; i<=array_length;i++){
+        String name_string;
+        name_string += "text_";
+        name_string+=courses[i];
+        components_text[i] ->name = name_string;
+
+        int n = name_string.length();
+        const char char_array[n+1];
+        strcpy(char_array, name_string.c_str());
+
+        components_text[i]-> nexText= NexText(1, i+2, char_array);
+    }
+
+}
+void initialize_course(String candidates [],int page_number, String course_name){
+    
+    int n = course_name.length();
+    const char name_char_array[n+1];
+    strcpy(name_char_array, course_name.c_str());
+
+    NexPage mechatronics_page = NexPage(page_number, 0, name_char_array);
+
+    int array_length = sizeof(candidates)/sizeof(candidates[0]);
+    candidate_components_text *components_text [array_length];
+     for(int  i = 0; i<=array_length;i++){
+        String name_string;
+        name_string += "text_";
+        name_string+=candidates[i];
+        components_text[i] ->name = name_string;
+
+        int n = name_string.length();
+        const char char_array[n+1];
+        strcpy(char_array, name_string.c_str());
+
+        components_text[i]-> nexText= NexText(page_number, i+1, char_array);
     }
 }
